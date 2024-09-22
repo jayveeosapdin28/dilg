3d6c4e1aa112418ba5161a1be25e3de78ebfc407
Jayvee Osapdin <34233093+jayveeosapdin28@users.noreply.github.com>
2024-09-22 18:19:22 +0800

    build

--- public/build/assets/useRole-DgTbftke.js
+++
@@ -1,1 +0,0 @@
-import{Q as s,m as o}from"./app-BKaUXmZh.js";function t(){const r=s();return{hasRole:e=>o(()=>r.props.auth.user.role.indexOf(e)!==-1).value,isInRole:e=>Array.isArray(e)?o(()=>e.includes(r.props.auth.user.role)).value:!1}}export{t as u};
